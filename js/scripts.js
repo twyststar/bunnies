e6e902b7dafb4ed0f09f5f5c07f69dada7f56b8a
Ben Petrosky and Starlynne Start <starlynne@>
2017-03-23 14:50:48 -0700

    debug jQuery if/else

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-// $(document).ready(function(){
-//   $(".clickable").click(function(){
-//       $(".cute").show();
-//   });
-// });
+$(document).ready(function(){
+  $("#bunnyForm").submit(function(event){
+    event.preventDefault();
+    var bunnyChoice = $("#bunny").val();
+    $(".info").hide();
+
+    if (bunnyChoice === "Cute"){
+      $("#cuteDiv").show();
+
+    } else if (bunnyChoice === "Toothy"){
+      $("#toothyDiv").show();
+
+    } else{
+      $("#fluffyDiv").show();
+
+    }
+
+  });
+});
